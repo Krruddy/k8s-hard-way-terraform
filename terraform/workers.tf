a8471acf6f0187bf6a4e45c55db4080b8ed1577b
Krruddy <rkinsiclounon@gmail.com>
2026-02-14 20:12:21 +0100

    Adds worker node provisioning to the infrastructure

---
+++ terraform/workers.tf
@@ -0,0 +1,43 @@
+resource "proxmox_virtual_environment_vm" "worker" {
+  count     = var.k8s_wkr_count
+  name      = "k8s-wkr-${count.index}"
+  node_name = var.proxmox_name
+  vm_id     = var.k8s_wkr_id_start + count.index
+
+  clone {
+    vm_id = var.template_vm_id
+    full  = false
+  }
+
+  # The QEMU Guest Agent used to retreive IP addresses and other info from the VM.
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = var.k8s_wkr_cpu_cores
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = var.k8s_wkr_memory
+  }
+
+  network_device {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  initialization {
+    datastore_id = var.proxmox_storage
+
+    ip_config {
+      ipv4 { address = "${cidrhost("10.0.0.0/24", 20 + count.index)}/24" }
+    }
+
+    user_account {
+      username = "admin"
+      keys     = [var.ssh_public_key]
+    }
+  }
+}
