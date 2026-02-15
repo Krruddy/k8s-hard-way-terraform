26a42058cf5d52dc991d45f016366aa8ab85ccb1
Krruddy <rkinsiclounon@gmail.com>
2026-02-15 01:43:44 +0100

    Adds automatic generation of Ansible inventory

---
+++ terraform/inventory.tf
@@ -0,0 +1,21 @@
+resource "local_file" "ansible_inventory" {
+  filename = "../ansible/inventory.ini"
+  content  = <<EOT
+[gateway]
+${proxmox_virtual_environment_vm.gateway.name} ansible_host=${split("/", var.k8s_gateway_wan_ip)[0]}
+
+[controllers]
+%{ for vm in proxmox_virtual_environment_vm.controllers ~}
+${vm.name} ansible_host=${split("/", vm.initialization[0].ip_config[0].ipv4[0].address)[0]}
+%{ endfor ~}
+
+[workers]
+%{ for vm in proxmox_virtual_environment_vm.workers ~}
+${vm.name} ansible_host=${split("/", vm.initialization[0].ip_config[0].ipv4[0].address)[0]}
+%{ endfor ~}
+
+[k8s_cluster:children]
+controllers
+workers
+EOT
+}
