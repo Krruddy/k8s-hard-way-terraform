f5b9b53f5c6f11e935a06b560b50b8c8739db71c
Krruddy <rkinsiclounon@gmail.com>
2026-02-21 09:20:17 +0100

    Updates SSH signing roles for clarity and forwarding support

--- terraform/ssh_backend.tf
+++ terraform/ssh_backend.tf
@@ -12,21 +12,21 @@ resource "vault_ssh_secret_backend_ca" "ssh_ca" {
 
 # Create the role used by the CD pipeline (Ansible) to sign its SSH keys
 resource "vault_ssh_secret_backend_role" "ansible" {
-  name                    = "ansible-role"
+  name                    = "ansible-signer"
   backend                 = vault_mount.ssh.path
   key_type                = "ca"
   allow_user_certificates = true
   allowed_users           = var.ssh_principal_ansible # This is a principal not a user
   default_user            = var.ssh_principal_ansible
   ttl                     = "30m"
-  allowed_extensions      = "permit-pty"
-  default_extensions      = { "permit-pty" : "" }
+  allowed_extensions      = "permit-pty,permit-port-forwarding"
+  default_extensions      = { "permit-pty" : "", "permit-port-forwarding" : "" }
   # Set allowed_user_key_config
 }
 
 # Create the role used by the dev team members to sign their SSH keys
 resource "vault_ssh_secret_backend_role" "dev_team" {
-  name                    = "dev-team-role"
+  name                    = "dev-team-signer"
   backend                 = vault_mount.ssh.path
   key_type                = "ca"
   allow_user_certificates = true
