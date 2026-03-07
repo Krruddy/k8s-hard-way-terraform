ad07e85e95a6e4e499f2441fb7b7612d23863f69
Krruddy <rkinsiclounon@gmail.com>
2026-03-07 13:15:14 +0100

    Adds etcd nodes group to inventory generation

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -9,6 +9,11 @@ ${proxmox_virtual_environment_vm.gateway.name} ansible_host=${split("/", var.k8s
 ${vm.name} ansible_host=${split("/", vm.initialization[0].ip_config[0].ipv4[0].address)[0]}
 %{ endfor ~}
 
+[etcd_nodes]
+%{ for vm in proxmox_virtual_environment_vm.controllers ~}
+${vm.name} ansible_host=${split("/", vm.initialization[0].ip_config[0].ipv4[0].address)[0]}
+%{ endfor ~}
+
 [workers]
 %{ for vm in proxmox_virtual_environment_vm.workers ~}
 ${vm.name} ansible_host=${split("/", vm.initialization[0].ip_config[0].ipv4[0].address)[0]}
@@ -23,5 +28,6 @@ localhost ansible_connection=local
 [k8s_cluster:children]
 controllers
 workers
+etcd_nodes
 EOT
 }
